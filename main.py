3c40ae9e21e2547fdb29e7ddc25ddca522c8d738
Tyler Cox <tyler.cox@expeditors.com>
2018-11-30 10:26:38 -0500

    Resolve Code

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 import os
 import json
 import google.oauth2.credentials
-import google_auth_oauthlib.flow
 import googleapiclient.discovery
+import google_auth_oauthlib.flow
 
 from flask import Flask, session, redirect, url_for, request, jsonify
 from datetime import date, datetime
@@ -36,8 +36,8 @@ def remove_empty_kwargs(**kwargs):
 
 @app.route('/')
 def index():
-  if 'credentials' not in session:
-    return redirect('authorize')
+    if 'credentials' not in session:
+        return redirect('authorize')
     #Keywords = 'red dead online glitch'
     #Keywords = 'black ops 4 glitches'
     #Keywords = 'black ops 4'
@@ -45,108 +45,105 @@ def index():
     #Keywords = 'fallout 76 glitch'
     #Keywords = 'red dead online guide'
     #Keywords = 'cwl update'
-  
 
-  return '<a href="/run/fallout glitches/">Run Fallout Glitches</a><a href="/run/red dead online glitch/">Run Red Dead Online Glitch</a></br>'
+    return '<a href="/run/fallout glitches/">Run Fallout Glitches</a><a href="/run/red dead online glitch/">Run Red Dead Online Glitch</a></br>'
 
 @app.route('/run/<string:search>/')
 def run(search):
-  
-  if 'credentials' not in session:
-    return redirect('authorize')
+    if 'credentials' not in session:
+        return redirect('authorize')
 
-  run_script(search)
+    run_script(search)
 
-  return 'Script Processed: ' + search
+    return 'Script Processed: ' + search
 
 @app.route('/authorize')
 def authorize():
-  # Create a flow instance to manage the OAuth 2.0 Authorization Grant Flow
-  # steps.
-  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-      CLIENT_SECRETS_FILE, scopes=SCOPES)
-  flow.redirect_uri = url_for('oauth2callback', _external=True)
-  print(flow)
-  authorization_url, state = flow.authorization_url(
-      # This parameter enables offline access which gives your application
-      # both an access and refresh token.
-      access_type='offline',
-      # This parameter enables incremental auth.
-      include_granted_scopes='true')
+    # Create a flow instance to manage the OAuth 2.0 Authorization Grant Flow
+    # steps.
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, scopes=SCOPES)
+    flow.redirect_uri = url_for('oauth2callback', _external=True)
+    print(flow)
+    authorization_url, state = flow.authorization_url(
+        # This parameter enables offline access which gives your application
+        # both an access and refresh token.
+        access_type='offline',
+        # This parameter enables incremental auth.
+        include_granted_scopes='true')
 
-  # Store the state in the session so that the callback can verify that
-  # the authorization server response.
-  session['state'] = state
+    # Store the state in the session so that the callback can verify that
+    # the authorization server response.
+    session['state'] = state
 
-  return redirect(authorization_url)
+    return redirect(authorization_url)
 
 
 @app.route('/oauth2callback')
 def oauth2callback():
-  # Specify the state when creating the flow in the callback so that it can
-  # verify the authorization server response.
-  state = session['state']
-  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-      CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
-  flow.redirect_uri = url_for('oauth2callback', _external=True)
+    # Specify the state when creating the flow in the callback so that it can
+    # verify the authorization server response.
+    state = session['state']
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+    flow.redirect_uri = url_for('oauth2callback', _external=True)
 
-  # Use the authorization server's response to fetch the OAuth 2.0 tokens.
-  authorization_response = request.url
-  
-  flow.fetch_token(authorization_response=authorization_response)
-  # Store the credentials in the session.
-  # ACTION ITEM for developers:
-  #     Store user's access and refresh tokens in your data store if
-  #     incorporating this code into your real app.
-  credentials = flow.credentials
-  session['credentials'] = {
-      'token': credentials.token,
-      'refresh_token': credentials.refresh_token,
-      'token_uri': credentials.token_uri,
-      'client_id': credentials.client_id,
-      'client_secret': credentials.client_secret,
-      'scopes': credentials.scopes
-  }
+    # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+    authorization_response = request.url
+    
+    flow.fetch_token(authorization_response=authorization_response)
+    # Store the credentials in the session.
+    # ACTION ITEM for developers:
+    #     Store user's access and refresh tokens in your data store if
+    #     incorporating this code into your real app.
+    credentials = flow.credentials
+    session['credentials'] = {
+        'token': credentials.token,
+        'refresh_token': credentials.refresh_token,
+        'token_uri': credentials.token_uri,
+        'client_id': credentials.client_id,
+        'client_secret': credentials.client_secret,
+        'scopes': credentials.scopes
+    }
 
-  return redirect(url_for('index'))
+    return redirect(url_for('index'))
 
 def run_script(Keywords):
-  # Load the credentials from the session.
-  credentials = google.oauth2.credentials.Credentials(
-      **session['credentials'])
+    # Load the credentials from the session.
+    credentials = google.oauth2.credentials.Credentials(
+        **session['credentials'])
 
-  client = googleapiclient.discovery.build(
-      API_SERVICE_NAME, API_VERSION, credentials=credentials)
+    client = googleapiclient.discovery.build(
+        API_SERVICE_NAME, API_VERSION, credentials=credentials)
 
+    searchList = search_list_by_keyword(client,
+                                part='snippet',
+                                maxResults=50,
+                                q=Keywords,
+                                type='video',
+                                publishedAfter=str(date.today()) + 'T00:00:00Z'
+                                #publishedAfter=str(date.today() - timedelta(days=30)) + 'T00:00:00Z'
+                            )
 
-  searchList = search_list_by_keyword(client,
-                          part='snippet',
-                          maxResults=50,
-                          q=Keywords,
-                          type='video',
-                          publishedAfter=str(date.today()) + 'T00:00:00Z'
-                          #publishedAfter=str(date.today() - timedelta(days=30)) + 'T00:00:00Z'
-                          )
+    channelIDs = getChannelIds(searchList)
+    videoIDs = getVideoIds(searchList)
+    
+    channels_list_by_id(client,
+        part='statistics',
+        id=channelIDs)
 
-  channelIDs = getChannelIds(searchList)
-  videoIDs = getVideoIds(searchList)
-  
-  channels_list_by_id(client,
-      part='statistics',
-      id=channelIDs)
+    videos_list_by_id(client,
+        part='contentDetails,statistics',
+        id=videoIDs)
 
-  videos_list_by_id(client,
-      part='contentDetails,statistics',
-      id=videoIDs)
-
-  return 'worked!'
+    return 'worked!'
 
 def channels_list_by_username(client, **kwargs):
-  response = client.channels().list(
-    **kwargs
-  ).execute()
+    response = client.channels().list(
+        **kwargs
+    ).execute()
 
-  return jsonify(**response)
+    return jsonify(**response)
 
 def search_list_by_keyword(client, **kwargs):
     # See full sample for function
